4555f1e5eb4650c025453546f7febe23ffaf3712
tanglingteng <1173534634@qq.com>
2026-08-20 13:54:59 +0800

    增加计时器，使程序每秒发出信号

--- my_robot_controller/my_robot_controller/my_first_node.py
+++ my_robot_controller/my_robot_controller/my_first_node.py
@@ -6,7 +6,11 @@ class MyNode(Node):
 
     def __init__(self):
         super().__init__('first_node')
-        self.get_logger().info('ROS')
+        self.create_timer(1.0, self.timer_callback)
+
+
+    def timer_callback(self):
+        self.get_logger().info("Hello")
 
 def main(args=None):
     rclpy.init(args=args)
